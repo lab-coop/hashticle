5f4c48dfe0a3ef100beef1940f6345cfb02c332a
Ádám Gólya <adam.stork@gmail.com>
2016-11-17 09:09:51 +0100

    feat(client/app): add store to the app

--- src/client/app.js
+++ src/client/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-function App(Root) {
+const middleware = [ thunk ]
+
+function App(Root, StoreService) {
+  const store = createStore(StoreService, applyMiddleware(...middleware));
+
   return (
-    <Root title="Read news by tags" />
+    <Provider store={store}>
+      <Root title="Read news by tags" />
+    </Provider>
   )
 }
 
-App.deps = ['Root'];
+
+App.deps = ['Root', 'Store'];
 module.exports = App;
