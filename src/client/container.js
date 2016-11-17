1921d15a3582e86b6faec105ed6245bdc990b87e
Ádám Gólya <adam.stork@gmail.com>
2016-11-17 09:08:33 +0100

    feat(client/container): register the new modules as dependency

--- src/client/container.js
+++ src/client/container.js
@@ -3,12 +3,20 @@
 const di = require('lab-di')();
 
 const app = require('./app');
+const store = require('./store');
 
 const root = require('./components/root');
 const newsList = require('./components/news-list');
 
+const news = require('./services/news');
+
+const newsActions = require('./actions/news');
+
 di.registerModule(app, 'App');
+di.registerModule(store, 'Store');
 di.registerModule(root, 'Root');
 di.registerModule(newsList, 'NewsList');
+di.registerModule(news, 'newsService');
+di.registerModule(newsActions, 'NewsActions');
 
 module.exports = di;
