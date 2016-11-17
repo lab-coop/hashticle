34b701d7d384ee9610dcc692dc2a1e7845e35f31
Ádám Gólya <adam.stork@gmail.com>
2016-11-17 10:30:09 +0100

    feat(features/support/world): add DOM to the world

--- src/features/support/world.js
+++ src/features/support/world.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+const jsdom = require('jsdom').jsdom;
 
 module.exports = function() {
   this.World = World;
@@ -9,5 +10,22 @@ module.exports = function() {
 function World() {
   this.context = {};
   this.container = require('../../client/container');
+  setupDOM();
 }
 
+function setupDOM() {
+  const exposedProperties = ['window', 'navigator', 'document'];
+
+  global.document = jsdom('');
+  global.window = document.defaultView;
+  Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+      exposedProperties.push(property);
+      global[property] = document.defaultView[property];
+    }
+  });
+
+  global.navigator = {
+    userAgent: 'node.js'
+  };
+}
