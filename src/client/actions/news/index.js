f3568a70343ff3fb666ed356ff5ae2a70cd003f3
Ádám Gólya <adam.stork@gmail.com>
2016-11-17 09:06:02 +0100

    feat(client/actions/news): initial add of news actions

---
+++ src/client/actions/news/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function NewsActions(NewsService) {
+
+  const REQUEST_NEWS = 'REQUEST_NEWS';
+  const RECEIVE_NEWS = 'RECEIVE_NEWS';
+
+  const requestNews = (hashtags) => ({
+    type: REQUEST_NEWS,
+    hashtags
+  });
+
+  const receiveNews = (hashtags, news) => ({
+    type: RECEIVE_NEWS,
+    hashtags,
+    news
+  });
+
+  const fetchNews = hashtags => dispatch => {
+    return NewsService.getByHashtags(hashtags)
+      .then(news => dispatch(receiveNews(hashtags, news)));
+  }
+
+  const getNews = hashtags => dispatch => {
+    dispatch(requestNews(hashtags));
+    dispatch(fetchNews(hashtags));
+  }
+
+  return Object.freeze({
+    RECEIVE_NEWS,
+    REQUEST_NEWS,
+    getNews
+  });
+
+}
+
+NewsActions.deps = ['newsService'];
+module.exports = NewsActions;
