b63fcbaf593a020bad8a5b0703c974629d6c52b0
Ádám Gólya <adam.stork@gmail.com>
2016-11-20 20:21:10 +0100

    feat(components/root/header): add basic style to the header div

--- src/client/components/root/header.js
+++ src/client/components/root/header.js
@@ -1,11 +1,12 @@
 'use strict';
 
 import React from 'react';
+import './header.scss';
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className="header-container light-text mdl-layout mdl-js-layout">
         <h1>{this.props.name}</h1>
         <h2>{this.props.title}</h2>
       </div>
