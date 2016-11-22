0c4d01f3865bd6d94b1f64a2d21eb3d3e7f8c8ea
Ádám Gólya <adam.stork@gmail.com>
2016-11-22 10:47:01 +0100

    fix(support/tools): increase the timeout
    The 5 second is the default timeout in cucumber, so here should be
    the same.

--- src/features/support/tools/index.js
+++ src/features/support/tools/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function waitForElement(element, selector, callback, timeout) {
-  timeout = timeout || 100;
+  timeout = timeout || 5000;
   const interval = 10;
   const timer = setInterval(function() {
     const foundElements = element.find(selector);
