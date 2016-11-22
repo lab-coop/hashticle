b3213c04a6aaf2cf511dda87312caa7c632d6556
Ádám Gólya <adam.stork@gmail.com>
2016-11-22 11:42:38 +0100

    reaftor(components/root/header): change the header of the index page

--- src/client/components/root/header.js
+++ src/client/components/root/header.js
@@ -6,9 +6,9 @@ import './header.scss';
 class Header extends React.Component {
   render() {
     return (
-      <div className="header-container light-text mdl-layout mdl-js-layout">
-        <h1>{this.props.name}</h1>
-        <h2>{this.props.title}</h2>
+      <div className="header-container">
+        <h1 className="mdl-typography--display-4">{this.props.name}</h1>
+        <span className="mdl-typography--headline">{this.props.title}</span>
       </div>
     );
   }
