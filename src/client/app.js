6d2ddc7efdef606162be6ca94d40249318e1755c
Ádám Gólya <adam.stork@gmail.com>
2016-11-23 15:37:05 +0100

    feat(client/app): add routing to the app
    We use createMemoryHistory to be able to test the code, on the other
    hand we can easily switch to server side rendering if its necessary.
    
    In the follow up patches we will introduce the login page where we
    need to use the router.

--- src/client/app.js
+++ src/client/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { Router, Route, createMemoryHistory } from 'react-router';
 
 const middleware = [ thunk ]
 
@@ -12,11 +13,13 @@ function App(Root, StoreService) {
 
   return (
     <Provider store={store}>
-      <Root title="Read news by tags" />
+      <Router history={createMemoryHistory()}>
+        <Route path="/" component={Root}>
+        </Route>
+      </Router>
     </Provider>
   )
 }
-
 
 App.deps = ['Root', 'Store'];
 module.exports = App;
