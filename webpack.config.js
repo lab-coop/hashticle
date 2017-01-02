09e07dfc478e2312788985ea9ffc2ed258805a13
Ádám Gólya <adam.stork@gmail.com>
2017-01-02 13:31:12 +0100

    chore(webpack.config.js): move the client build to dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const DIST_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src/client');
 const PUBLIC_DIR = path.resolve(APP_DIR, 'public');
-const DIST_DIR = path.resolve(PUBLIC_DIR, 'dist');
+const CLIENT_PUBLIC_DIR = path.resolve(DIST_DIR, 'public');
+const CLIENT_DIST_DIR = path.resolve(CLIENT_PUBLIC_DIR, 'dist');
 
 const client = {
   entry: ['babel-polyfill', APP_DIR + '/index.js'],
   output: {
-    filename: DIST_DIR + '/bundle.js',
+    filename: CLIENT_DIST_DIR+ '/bundle.js',
     publicPath: PUBLIC_DIR
   },
   node: {
@@ -35,7 +38,11 @@ const client = {
     }]
   },
   plugins: [
-    new ExtractTextPlugin(DIST_DIR + '/bundle.css')
+    new ExtractTextPlugin(CLIENT_DIST_DIR + '/bundle.css'),
+    new CopyWebpackPlugin([{
+      from: path.resolve(PUBLIC_DIR, 'index.html'),
+      to: CLIENT_PUBLIC_DIR}
+    ])
   ]
 };
 
